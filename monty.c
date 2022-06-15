c823a0f339d22e1086722b29820ae877913e2dd4
Nabil Affo <nabil.noble01@gmail.com>
2022-06-15 09:23:06 +0000

    Add monty.c file: main source file (uncomplete for now)

---
+++ monty.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the monty interpreter
+ * @argc: arguments counts
+ * @argv: array of arguments
+ *
+ * Return: EXIT_SUCCESS if success and
+ *	EXIT_FAILURE if failure
+ */
+int main(int argc, char *argv[])
+{
+	args_s args;
+
+	args.ac = argc;
+	args.bcfile = argv[1];
+	args.line_number = 0;
+	montymain(&args);
+	return (EXIT_SUCCESS);
+
+}
+
+/**
+ * montymain - proper main function
+ * @arg: pointer to arguments of main build in struct
+ *
+ * Return: Nothing
+ */
+void montymain(args_t *arg)
+{
+	/* if more than 1 argument */
+	if (arg->ac != 2)
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+}
