8b373cf5a752ef4aad8dfbfcf7cde44680f662de
Maysummer <udojyk@gmail.com>
2022-06-16 06:21:16 -0700

    pop

--- opcodes1.c
+++ opcodes1.c
@@ -90,7 +90,7 @@ void monty_pop(stack_t **stack, unsigned int line_number)
 
 	if ((*stack) == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		free_everything();
 		exit(EXIT_FAILURE);
 	}
