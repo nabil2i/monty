ba94fdbe90a951ca7fbd9416025d38336d44c7aa
Maysummer <udojyk@gmail.com>
2022-06-16 04:21:29 -0700

    pint

--- opcodes1.c
+++ opcodes1.c
@@ -77,7 +77,11 @@ void monty_pint(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 
 	if ((*stack)->next == NULL)
-		print_list(*stack);
+	{
+		fprintf(stderr, "L %d:  can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->next->n);
 }
 
 /**
