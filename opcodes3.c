7ec40f7176928eaed6aed1ca2441fab53591da6c
Nabil <nabil.noble01@gmail.com>
2022-06-16 17:25:33 +0000

    Add code for monty_mod

--- opcodes3.c
+++ opcodes3.c
@@ -9,8 +9,34 @@
  */
 void monty_mod(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
+	size_t nodes;
+	int mod = 0;
+	stack_t *new;
+
+	nodes = count_nodes(stack);
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		free_everything();
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_everything();
+		exit(EXIT_FAILURE);
+	}
+	mod = (*stack)->next->n % (*stack)->n;
+	remove_top(stack);
+	remove_top(stack);
+
+	new = add_node_top(stack, mul);
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_everything();
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
