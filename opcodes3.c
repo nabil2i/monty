558d78979ae7c8166dc7d22c9a408dc4b6afe9f6
Nabil <nabil.noble01@gmail.com>
2022-06-17 05:46:11 +0000

    Fix betty style for opcodes3.c

--- opcodes3.c
+++ opcodes3.c
@@ -1,7 +1,8 @@
 #include "monty.h"
 
 /**
- * monty_mod - multiply the second top element with the top element of the stack
+ * monty_mod - multiply the second top element with
+ *	the top element of the stack
  * @stack: double pointer to the stack
  * @line_number: current line
  *
